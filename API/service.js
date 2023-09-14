4b61d1b26806e833d4898db0e205c5cf03dcc0f2
Miseon <soup7912@gmail.com>
2023-09-14 19:30:24 +0900

    컨테이너 생성 요청 함수 작성

--- API/service.js
+++ API/service.js
@@ -8,17 +8,41 @@ const db =require('../db.js');
 router.use(bodyParser.urlencoded({extended : true}));
 router.use(bodyParser.json()); 
 
+// shell 명령어 실행 module
+const shell = require('shelljs')
+
+// python file을 실행하여 컨테이너 생성 요청
+async function createContainerScript(info) {
+    shell.cd('~');
+
+    // 실행할 shell 명령어
+    let script = `
+        python3 script.py ${info.usrename} ${info.os} ${info.cpu} ${info.ram} ${info.volume} ${info.volumeCount}
+    `
+
+    if(shell.exec(script).code !== 0) {
+        console.log("fail");
+        shell.exit(1);
+    }
+}
 
 router.post('/service',function(req,res){
-    var username = req.body.username;
-    var os =req.body.os;
-    var cpu = req.body.cpu;
-    var ram  = req.body.ram;
-    var gpu = req.body.gpu;
-    var gpuCompany = req.body.gpuCompany;
-    var volume = req.body.volume;
-    var volumeCount =req.body.volumeCount;
-    var serviceNum = req.body.serviceNum;
+    let username = req.body.username;
+    let os =req.body.os;
+    let cpu = req.body.cpu;
+    let ram  = req.body.ram;
+    let gpu = req.body.gpu;
+    let gpuCompany = req.body.gpuCompany;
+    let volume = req.body.volume;
+    let volumeCount =req.body.volumeCount;
+    let serviceNum = req.body.serviceNum;
+
+    // 생성할 컨테이너 정보
+    let createContainerInfo = {
+        username, os, cpu, ram, volume, volumeCount
+    }
+
+    createContainerScript(createContainerInfo);
 
     db.query('SELECT * FROM servicetable WHERE username = ?',[username], function(err,results,fields){
         if(err) throw error;
